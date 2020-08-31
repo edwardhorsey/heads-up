2f45fa611de6ff751f6e79a846b925720e99220b
Edward Horsey <edwardhorsey@me.com>
2020-08-31 09:33:36 +0100

    housework added react app

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Heads Up Poker</p>
+    </div>
+  );
+}
+
+export default App;
