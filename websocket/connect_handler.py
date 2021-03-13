bfb7774bd2df39423ec8a3e4be2442f6351dfed6
edwardhorsey <edwardhorsey@me.com>
2021-03-13 18:24:52 +0000

    remove connected response

--- websocket/connect_handler.py
+++ websocket/connect_handler.py
@@ -10,21 +10,4 @@ def handle(event, context):
     # Insert the Id and default name of the connected user to the database
     dynamodb.put_item(TableName=os.environ['POKER_CONNECTIONS_TABLE_NAME'], Item={'connectionId': {'S': connectionId}, 'name': {'S': ''}})
 
-    response = {
-        'method': 'connected',
-        'uid': connectionId
-    }
-
-    endpoint = 'http://localhost:3001/' if 'IS_OFFLINE' in os.environ else ("https://"
-        + event["requestContext"]["domainName"]
-        + "/"
-        + event["requestContext"]["stage"])
-
-    # Emit response back to user
-    apigatewaymanagementapi = boto3.client('apigatewaymanagementapi', endpoint_url = endpoint)
-    apigatewaymanagementapi.post_to_connection(
-        Data=json.dumps(response),
-        ConnectionId=connectionId
-    )
-
     return {}
