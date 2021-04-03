9160652ba8044cc3ce9c26bbda7c91c4c455684c
Edward Horsey <edwardhorsey@protonmail.com>
2021-04-03 23:02:55 +0100

    copytoclipboard: copy

--- frontend/src/Components/WaitingRoom/WaitingRoom.tsx
+++ frontend/src/Components/WaitingRoom/WaitingRoom.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import styles from './WaitingRoom.module.scss';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useServer } from '../../Context/serverContext';
+import styles from './WaitingRoom.module.scss';
 import Button from '../Button';
 
 const WaitingRoom: React.FC = () => {
   const { gameState, gameDispatch } = useServer();
   const { displayName, gid } = gameState;
+  const [clipboard, setClipboard] = useState({ copied: false });
+
   return (
     <section className={styles.WaitingRoom}>
       <Button logic={() => gameDispatch({ type: 'removeGid' })} text="Back" />
       <p>{displayName}</p>
-      <p>
+      <div>
         Game ID:
         {gid}
-      </p>
+        <CopyToClipboard
+          text={gid}
+          onCopy={() => setClipboard({ copied: true })}
+        >
+          <span>Copy to clipboard</span>
+        </CopyToClipboard>
+      </div>
+      {clipboard.copied ? <span style={{ color: 'red' }}>Copied.</span> : null}
       <p>Waiting for second player...</p>
     </section>
   );
