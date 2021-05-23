d0e650d73881e3d0a342e43cd06a9fa799bf9630
Edward Horsey <edwardhorsey@protonmail.com>
2021-05-23 20:15:08 +0100

    feature/missing-chip-icon: adjust login page chip stack count.

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -5,7 +5,7 @@ import ChipsGen from '../ChipsGen';
 const Login: React.FC = () => (
   <section className={styles.Login}>
     <h1>Welcome to Poker</h1>
-    <ChipsGen amount={49001} />
+    <ChipsGen amount={49000} />
     <a href={process.env.REACT_APP_COGNITO_LOGIN_URL}>Login</a>
   </section>
 );
